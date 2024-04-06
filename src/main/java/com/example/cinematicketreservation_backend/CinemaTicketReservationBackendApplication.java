01b02534bccd7c9b28d51f40b81c319ab9b658d6
jabbour-lahoucine <jabbour.lahoucine@gmail.com>
2024-04-06 21:46:00 +0000

    new files configuration

---
+++ src/main/java/com/example/cinematicketreservation_backend/CinemaTicketReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinematicketreservation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketReservationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaTicketReservationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinematicketreservation_backend/CinemaTicketReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinematicketreservation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketReservationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
